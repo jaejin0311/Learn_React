9ba14a75de8836a81b33cde43643cc14df7bc87f
Jaejin Kim <jaejink@uci.edu>
2023-03-27 19:50:42 -0700

    To do List Implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setTodos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setTodos((prev) => [toDo, ...prev]);
+    setToDo("");
+  };
+  
   return (
     <div>
-      <h1>Welcome Back!</h1>
+      <h1>My To Dos {toDos.length} </h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange ={onChange} 
+          type="text" 
+          value={toDo} 
+          placeholder="Write your to do">
+        </input>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
